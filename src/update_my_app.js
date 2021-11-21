5f49057baab2552f783fa5227feedcb77fd059d7
Chidozie <codingwithcn@gmail.com>
2021-11-21 01:31:46 -0500

    Fixing update notification

--- src/update_my_app.js
+++ src/update_my_app.js
@@ -1,17 +1,17 @@
 // wait for an updateReady message
 render.on('updateReady', function(event, text) {
     // changes the text of the button
-    ipcRenderer.send('app_version');
+    render.send('app_version');
 });
 
-render.on("app_version", function(event, data){
+render.on("app_version", function(event, data) {
     var show_update_div = document.getElementById("show_update");
 
     show_update_div.innerHTML = `
     <div style="width: 160px; background-color: blue; color: white; text-align: center;border-radius: 6px; padding: 8px 0; position: absolute; z-index: 1; bottom: 50%; left: 50%; margin-left: -80px;">
         <p>You are currently on Version: ${data.version}</p>    
         <p>And a new update for Captalist is now availiable</p>
-        <button onClick="ipcRenderer.send('quitAndInstall')">Update Now!!!</button>
+        <button onClick="render.send('quitAndInstall')">Update Now!!!</button>
     </div>
     `
 });
